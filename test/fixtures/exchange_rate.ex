11fedb6cdff630b8aa533e0c41993155996774d9
Alan Nascimento <alanoliveiran45@gmail.com>
2021-12-01 22:44:58 -0300

    Add fixture to ExchangeRate response

---
+++ test/fixtures/exchange_rate.ex
@@ -0,0 +1,18 @@
+defmodule CurrencyConversion.Fixtures.ExchangeRate do
+  @moduledoc """
+  Fixtures for HTTP responses from ExchangeRate
+  """
+
+  def success_converted(%{from: from, to: to, amount: amount, result: result}) do
+    ~s<
+    {
+      "date": "2021-11-30",
+      "historical": false,
+      "info": {"rate": 5.6},
+      "query": {"amount": #{amount}, "from": "#{from}", "to": "#{to}"},
+      "result": #{result},
+      "success": true
+    }
+    >
+  end
+end
