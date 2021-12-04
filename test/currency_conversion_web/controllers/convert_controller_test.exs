4ee075a75d0432da32450250456253a8f76929fa
Alan Nascimento <alanoliveiran45@gmail.com>
2021-12-04 16:56:40 -0300

    Use Bypass in ConvertControllerTest

--- test/currency_conversion_web/controllers/convert_controller_test.exs
+++ test/currency_conversion_web/controllers/convert_controller_test.exs
@@ -1,6 +1,25 @@
 defmodule CurrencyConversionWeb.ConvertControllerTest do
   use CurrencyConversionWeb.ConnCase
 
+  alias CurrencyConversion.Support.Fixtures
+
+  setup do
+    bypass = Bypass.open()
+
+    Application.put_env(:currency_conversion, :exchange_rate,
+      endpoint: "http://localhost:#{bypass.port}/"
+    )
+
+    params = %{
+      from: "USD",
+      to: "BRL",
+      amount: 10,
+      result: 56.01
+    }
+
+    {:ok, bypass: bypass, params: params}
+  end
+
   describe "GET /api/convert" do
     test "with invalid base_currency, it returns an error message", %{conn: conn} do
       response =
@@ -21,8 +40,16 @@ defmodule CurrencyConversionWeb.ConvertControllerTest do
     end
 
     test "with valid params received, it returns the message with the amount converted", %{
-      conn: conn
+      conn: conn,
+      bypass: bypass,
+      params: params
     } do
+      request_response = Fixtures.ExchangeRate.success_converted(params)
+
+      Bypass.expect(bypass, fn conn ->
+        Plug.Conn.send_resp(conn, 200, request_response)
+      end)
+
       response =
         conn
         |> get(Routes.convert_path(conn, :show), %{from: "USD", to: "BRL", amount: 10})
@@ -32,8 +59,8 @@ defmodule CurrencyConversionWeb.ConvertControllerTest do
         "base_currency" => "USD",
         "base_amount" => 10,
         "target_currency" => "BRL",
-        "converted_amount" => 56.53,
-        "message" => "10 USD -> 56.53 BRL"
+        "converted_amount" => 56.01,
+        "message" => "10 USD -> 56.01 BRL"
       }
 
       assert expected_response == response
