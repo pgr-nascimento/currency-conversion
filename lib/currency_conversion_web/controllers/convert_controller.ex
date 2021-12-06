64f33d3d00cc986a5ca120c946b7ef2a294b2aa3
Alan Nascimento <alanoliveiran45@gmail.com>
2021-12-06 20:25:41 -0300

    Refact show action
    I've changed the action to remove the way that I was controlling the flow, using with and the case was weird to me,
    I noticed that is possible to use the `with` with multiple clauses, and now I think it's better.

--- lib/currency_conversion_web/controllers/convert_controller.ex
+++ lib/currency_conversion_web/controllers/convert_controller.ex
@@ -3,23 +3,25 @@ defmodule CurrencyConversionWeb.ConvertController do
 
   alias CurrencyConversion.{Currency, ExchangeRate, Params}
 
-  action_fallback CurrencyConversionWeb.FallbackController
-
   def show(conn, wire_params) do
     params = Params.parse(wire_params)
 
-    with {:ok, ^params} <- Currency.validate(params) do
-      case ExchangeRate.convert(params) do
-        {:ok, result} ->
-          conn
-          |> put_status(:ok)
-          |> json(result)
+    with :ok <- Currency.validate(params),
+         {:ok, result} <- ExchangeRate.convert(params) do
+      conn
+      |> put_status(:ok)
+      |> json(result)
+    else
+      {:error, {:invalid_currency, message}} ->
+        conn
+        |> put_status(:bad_request)
+        |> put_view(CurrencyConversionWeb.ErrorView)
+        |> render("400.json", error: message)
 
-        {:error, _} ->
-          conn
-          |> put_status(:internal_server_error)
-          |> json(%{error: "Something unexpected happen. Try again"})
-      end
+      {:error, _} ->
+        conn
+        |> put_status(:internal_server_error)
+        |> json(%{error: "Something unexpected happen. Try again"})
     end
   end
 end
